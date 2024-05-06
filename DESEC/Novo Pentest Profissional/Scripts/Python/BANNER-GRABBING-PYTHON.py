ca068af6ff16d07fd9c0b1ff3bb031a84ef1bbc7
Gabriel Godoy <107765540+Godoy-png@users.noreply.github.com>
2024-05-06 08:46:58 -0300

    Add files via upload

---
+++ DESEC/Novo Pentest Profissional/Scripts/Python/BANNER-GRABBING-PYTHON.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import socket
+
+ip = input("Digite o IP:")
+porta = int(input("Digite a porta:"))
+
+meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+meusocket.connect((ip,porta))
+banner = meusocket.recv(1024)
+print(banner)
